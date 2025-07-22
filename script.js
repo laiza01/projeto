da75b38328ee681fee81023991c93b5d7a3e2845
laiza <laizabredacomputador@gmail.com>
2025-07-22 16:00:58 -0300

    script.js

--- script.js
+++ script.js
@@ -13,22 +13,5 @@ function toggleMode() {
    // se tiver sem light mode, manter a imagem normal
    img.setAttribute("src", "./assets/avatar.png")
   } 
- 
-}
- function toggleMode() {
-   const html = documentElement
- html.classList. toggle ("light")
-
- // pegar o texto 
-  const texto = document.querySelector ("#profile texto")
-
-  // substituir o texto
-  if (html.classList.contains("light")) {
-   // se tiver light mode, adicionar o texto light
-   Text.setAttribute("alt", "foto de Maike Brito sorrindo, usando oculus escuro e jaqueta preta, barba e fundo azul")
-  } else {
-   // se tiver sem light mode, manter o texto normal
-   Text.setAttribute("alt","foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo") 
  }
   
- }
